0eff4e9ee7cd072ba82a324232eaa7df7bde9ce1
Mr.Rahul Birajdar <rbirajadar96@gmail.com>
2026-07-11 17:21:26 +0530

    fix: revert axios baseURL to hardcoded URL

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-console.log(import.meta.env.VITE_API_URL)
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://ai-careear-giud-1.onrender.com/api",
     withCredentials: true,
 });
 
