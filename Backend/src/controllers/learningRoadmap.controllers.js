e175409f15b3f3c5859b553827166788c422f993
Mr.Rahul Birajdar <rbirajadar96@gmail.com>
2026-07-20 08:23:07 +0530

    fix: update roadmap response to return created roadmap data instead of generated roadmap

--- Backend/src/controllers/learningRoadmap.controllers.js
+++ Backend/src/controllers/learningRoadmap.controllers.js
@@ -2,6 +2,7 @@ import asyncHandler from "../utils/asycnHandler.js";
 import ApiErrorHandling from "../utils/ApiErrorHandling.js";
 import { ApiResponse } from "../utils/ApiResponceHandling.js";
 import generateRoadmap from "../services/generateRoadMap.service.js";
+import { Roadmap } from "../models/learningRoadmap.models.js";
 
 
 
@@ -13,10 +14,26 @@ const learningRoadmap = asyncHandler(async (req, res, next) => {
     if (!goal || !exp || !hours || !duration || !focus) {
         throw new ApiErrorHandling(400, "All Field Are Required !!")
     }
+    console.log({
+        goal,
+        exp,
+        hours,
+        duration,
+        focus,
+    });
     const getRoadmap = generateRoadmap(goal, exp, hours, duration, focus);
     console.log(getRoadmap);
+
+    const roadmapData = await Roadmap.create({
+        user: req.user._id,
+        goal: goal,
+        experience: exp,
+        estimatedTime: duration,
+        ...getRoadmap,
+    })
+
     return res.status(200).json(
-        new ApiResponse(200, getRoadmap, "Roadmap generated successfully")
+        new ApiResponse(200, roadmapData, "Roadmap generated successfully")
     );
 });
 
